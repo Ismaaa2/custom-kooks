04e54087a98f6ac28a23bc3bbfecf358a10f594b
Ismael <ismaaeelbonilla@hotmail.com>
2021-10-18 12:27:32 +0200

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,23 @@
+
+
+import { useState } from "react";
+
+
+export const useForm = ( initialState = {} ) => {
+
+    const [valorForm, setValorForm ] = useState(initialState);
+
+
+    const handleInputChange = ({ target }) => {
+        setValorForm({
+          ...valorForm,
+          [target.name]: target.value,
+        });
+      };
+
+    const reset = (reset = initialState) => {
+      setValorForm(reset)
+    }
+
+    return [ valorForm, handleInputChange, reset ]
+}
